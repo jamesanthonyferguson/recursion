3cf8f2a80e6462ffbd4ed3b1be4b12fe4eb9f69e
jamesanthonyferguson <jamesanthonyferguson@gmail.com>
2014-08-26 10:50:38 +1000

    getElementsByClassName Complete  - walk dom method

--- src/getElementsByClassName.js
+++ src/getElementsByClassName.js
@@ -6,4 +6,24 @@
 // But instead we're going to implement it from scratch:
 var getElementsByClassName = function(className){
   // your code here
+  var output = [];
+	
+	var walkDom = function(node) {
+		var next = node.childNodes;
+		classCheck(node);
+		for (var i = 0; i < next.length; i++) {
+			walkDom(next[i]);
+		}
+	}
+	
+	var classCheck = function(node) {
+		if (node.classList) {
+			if (node.classList.contains(className)) {
+				output.push(node);
+			}
+		}
+	}
+
+	walkDom(document.body);
+	return output;
 };
